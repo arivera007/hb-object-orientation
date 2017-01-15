22c9b841ff04422b8ae930a1442d241157b18ad2
Adriana Rivera <letsgotravel007-github@yahoo.com>
2017-01-15 20:48:43 +0000

    Questions answered

--- assessment.py
+++ assessment.py
@@ -3,21 +3,36 @@ Part 1: Discussion
 
 1. What are the three main design advantages that object orientation
    can provide? Explain each concept.
-   Abstraction: Get all the behavior in 
-   Encapsulation:
-   Polimorphism:
+   Abstraction: Generalization of things, categorizing, abstract ideas. Like, here are all the animals. 
+   Encapsulation: Hiding all the inner workings of an  object. Giving only the minimum necesary interface for user to operate such object.
+   Polimorphism: When given an order of the same kind to different kinds of objects, an object provides its own unique behavior according to its nature.
+   Inheritance: I would argue that inheritance is also a main advantage for design in object orientation. Though not mentioned in class as a pilar, it
+               would be hard to have abstraccion without inheritance.
 
 2. What is a class?
+   A collection of members that share behaviors and attributes.
    
    
 3. What is an instance attribute?
+   When one of the members of a class, an instance, has an specific attribute that is not shared necessarily amongst the other members 
+of the same class.
 
 4. What is a method?
+   It is a function that belongs to a class. Performs a certain behavior that will be shared amongs members of the same class.
 
 5. What is an instance in object orientation?
+   Once a class is defined, we can construct a new object base on the class 'blue print'. This object is the actual running code with specific
+   attributes.
 
 6. How is a class attribute different than an instance attribute?
    Give an example of when you might use each.
+   Class attribute MUST be shared by all instances of said class, and has the same value in all instances. Instance attributse may or may 
+   not be shared by all, but its value is tied only to said instance. Different instances may have same attribute, but it will most likely be
+   different in all of them. Some attibutes might exits in one instance and not in another.
+   Example: 
+      Fox class      -> food_type      (all foxes in the wild have the same type of food)          Class attribute
+      A fox instance -> qty_of_food    (but not all foxes can eat the same amount of that food)    Instance attribute
+      Another fox    -> broken_leg     (This fox has an extra attibute because of broken leg)      Instance attribute
 
 
 """
@@ -72,7 +87,7 @@ class Exam(object):
 def Quiz(Exam):
    
    def administer(self):             # Inheriting from Exam, but overriding administer(). Otherwise behaves the same.
-      return True if super(Quiz, self).administer() > 50 else False     # Quiz returns T or F instead of a score. Exam does the scoring.
+      return True if super(Quiz, self).administer() > 50 else False     # Quiz returns T or F if score >50 %. Exam does the scoring.
 
 
 def take_test(exam, student):
@@ -107,3 +122,5 @@ def example():
 if __name__ == "__main__":
    
    example()
+   
+   
